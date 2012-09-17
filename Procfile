b1d2ad1cab0288c862fe302269d735c0de069431
Jorg Jenni <jorg.jenni@jennius.co.uk>
2012-09-17 10:32:36 +0100

    init

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: java -cp target/classes:"target/dependency/*" uk.co.jennius.textparser.Main
+
---
+++ src/main/java/uk/co/jennius/textparser/Main.java
@@ -0,0 +1,23 @@
+package uk.co.jennius.textparser;
+
+import javax.servlet.http.*;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.*;
+
+
+public class Main extends HttpServlet {
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public static void main(String[] args) throws Exception{
+        Server server = new Server(Integer.valueOf(System.getenv("PORT")));
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.setContextPath("/");
+        server.setHandler(context);
+        context.addServlet(new ServletHolder(new TextParser()),"/*");
+        server.start();
+        server.join();   
+    }
+}
